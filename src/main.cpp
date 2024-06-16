db888fc739a01239274a37e78bcd1f5ee95425d2
Trim Bresilla <trim.bresilla@gmail.com>
2024-06-16 22:16:47 +0200

    feat: enhance GStreamer integration and build process
    - Add GStreamer as a required package
    - Fix a comment section in FindGStreamer.cmake
    - Comment out a line in CMakeLists.txt
    - Add the `pack` executable installation section in CMakeLists.txt
    - Add a new cmake file for finding GStreamer
    - Add device information output in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,28 @@ int main(int argc, char **argv){
     arv_update_device_list();
     //get num devices
     int num_devices = arv_get_n_devices();
-    std::cout << "Number of devices: " << num_devices << std::endl;
-    return 0;
+    for(unsigned int i = 0; i < num_devices; i++){
+        std::cout << "Device: " << arv_get_device_id(i) << std::endl;
+        std::cout << "Physical ID: " << arv_get_device_physical_id(i) << std::endl;
+        std::cout << "Address: " << arv_get_device_address(i) << std::endl;
+        std::cout << "Vendor: " << arv_get_device_vendor(i) << std::endl;
+
+        const char *device_id = arv_get_device_id(i);
+        ArvDevice *device = arv_open_device(device_id, NULL);
+
+        if(device == NULL){
+            std::cerr << "Failed to open device" << std::endl;
+            return 1;
+        }
+
+        ArvGc *gc = arv_device_get_genicam(device);
+
+        if(gc == NULL){
+            std::cerr << "Failed to get Genicam" << std::endl;
+            return 1;
+        }
+        
+    }
+
+
 }

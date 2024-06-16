90dcf37bfa59d887f7a8fa2d4bd9cbe7a2b8ffa3
Trim Bresilla <trim.bresilla@gmail.com>
2024-06-16 11:35:17 +0200

    feat: implement GLIB version detection for aravis device list update
    - Add GLIB as a required package in CMakeLists.txt
    - Create a new file FindGLIB.cmake under cmake directory
    - Implement the FindGLIB.cmake file with GLIB version detection
    - Include FindGLIB.cmake in the CMakeLists.txt file
    - Update main.cpp to include aravis device list update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <aravis-0.8/arv.h>
 
-int main(int argc, char **argv){
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
+
+int main(int argc, char **argv)
+{
+    arv_update_device_list();
+    return 0;
 }

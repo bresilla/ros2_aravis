dc6c2fa053bbc2db8b59756b29abc66d0225ba90
Trim Bresilla <trim.bresilla@gmail.com>
2024-06-16 21:10:17 +0200

    refactor: CMakeList updated to find glibconfig
    - Refactor `main.cpp` to get and output the number of connected devices

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,10 @@
 #include <aravis-0.8/arv.h>
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     arv_update_device_list();
+    //get num devices
+    int num_devices = arv_get_n_devices();
+    std::cout << "Number of devices: " << num_devices << std::endl;
     return 0;
 }
